49284fb1b6f7c74398ab5c77cbebff83a3f24a5f
Ludvig Karlsson <ludvig.karlsson@maxm.se>
2025-12-09 16:27:02 +0100

    Day Nine

--- nine/src/main.rs
+++ nine/src/main.rs
@@ -1,7 +1,7 @@
 use std::{path::Path, u64};
 
 use clap::Parser;
-use utils::{Args, Grid};
+use utils::Args;
 
 fn get_tuple(s: &str) -> Option<(usize, usize)> {
     s.split_once(",").and_then(|(ls, rs)| {
@@ -12,58 +12,139 @@ fn get_tuple(s: &str) -> Option<(usize, usize)> {
     })
 }
 
-fn part_two(points: &[(usize, usize)]) -> u64 {
-    let mut min_x = usize::MAX;
-    let mut max_x: usize = 0;
-    let mut min_y = usize::MAX;
-    let mut max_y: usize = 0;
+fn is_contained(points: &[(usize, usize)], point: (usize, usize)) -> bool {
+    let n = points.len();
 
-    for &(x, y) in points {
-        if x > max_x {
-            max_x = x;
-        }
-        if x < min_x {
-            min_x = x;
-        }
+    // Check edge
+    for i in 0..n {
+        let (x1, y1) = points[i];
+        let (x2, y2) = points[(i + 1) % n];
 
-        if y > max_y {
-            max_y = y;
-        }
-        if y < min_y {
-            min_y = y;
-        }
-    }
-
-    let mut grid = Grid::fill((max_y - min_y) + 1, (max_x - min_x) + 1, '.');
-    for i in 0..points.len() {
-        let next = if i == points.len() - 1 { 0 } else { i + 1 };
-
-        let (x, y) = points[i];
-        let (x_next, y_next) = points[next];
-
-        let row_min = y.min(y_next);
-        let row_max = y.max(y_next);
-        let col_min = x.min(x_next);
-        let col_max = x.max(x_next);
-
-        for row in row_min..=row_max {
-            for col in col_min..=col_max {
-                grid.set(row - min_y, col - min_x, 'X');
+        if y1 == y2 && point.1 == y1 {
+            let min_x = x1.min(x2);
+            let max_x = x1.max(x2);
+            if point.0 >= min_x && point.0 <= max_x {
+                return true;
             }
         }
 
-        grid.set(y - min_y, x - min_x, '#');
-        grid.set(y_next - min_y, x_next - min_x, '#');
-    }
-
-    for i in 0..grid.rows() {
-        for j in 0..grid.cols() {
-            print!("{}", grid.get(i, j).unwrap());
+        if x1 == x2 && point.0 == x1 {
+            let min_y = y1.min(y2);
+            let max_y = y1.max(y2);
+            if point.1 >= min_y && point.1 <= max_y {
+                return true;
+            }
         }
-        println!();
     }
 
-    0
+    let mut crossings = 0;
+    // Check inside
+    for i in 0..n {
+        let (x1, y1) = points[i];
+        let (x2, y2) = points[(i + 1) % n];
+
+        if x1 != x2 {
+            continue;
+        }
+        let x = x1;
+
+        let y_min = y1.min(y2);
+        let y_max = y1.max(y2);
+
+        if point.1 > y_min && point.1 <= y_max && point.0 < x {
+            crossings += 1;
+        }
+    }
+
+    crossings % 2 == 1
+}
+
+fn rect_cuts_boundary(
+    points: &[(usize, usize)],
+    xmin: usize,
+    xmax: usize,
+    ymin: usize,
+    ymax: usize,
+) -> bool {
+    let n = points.len();
+
+    for i in 0..n {
+        let (x1, y1) = points[i];
+        let (x2, y2) = points[(i + 1) % n];
+
+        if x1 == x2 {
+            let x = x1;
+            let (ylow, yhigh) = if y1 < y2 { (y1, y2) } else { (y2, y1) };
+
+            if x > xmin && x < xmax {
+                if ylow < ymin && yhigh > ymin {
+                    return true;
+                }
+                if ylow < ymax && yhigh > ymax {
+                    return true;
+                }
+            }
+        } else {
+            let y = y1;
+            let (xlow, xhigh) = if x1 < x2 { (x1, x2) } else { (x2, x1) };
+
+            if y > ymin && y < ymax {
+                if xlow < xmin && xhigh > xmin {
+                    return true;
+                }
+                if xlow < xmax && xhigh > xmax {
+                    return true;
+                }
+            }
+        }
+    }
+
+    false
+}
+
+fn rectangle_contained(points: &[(usize, usize)], p1: (usize, usize), p2: (usize, usize)) -> bool {
+    let (x1, y1) = p1;
+    let (x2, y2) = p2;
+
+    let xmin = x1.min(x2);
+    let xmax = x1.max(x2);
+    let ymin = y1.min(y2);
+    let ymax = y1.max(y2);
+
+    let corners = [(xmin, ymin), (xmin, ymax), (xmax, ymin), (xmax, ymax)];
+
+    if !corners.iter().all(|p| is_contained(&points, *p)) {
+        return false;
+    };
+
+    if rect_cuts_boundary(points, xmin, xmax, ymin, ymax) {
+        return false;
+    }
+
+    true
+}
+
+fn part_two(points: &[(usize, usize)]) -> u64 {
+    let mut max_area = 0;
+    let n = points.len();
+    for i in 0..n {
+        for j in i + 1..n {
+            let (xi, yi) = points[i];
+            let (xj, yj) = points[j % n];
+
+            let dx = ((xi as i64) - (xj as i64)).abs() as usize + 1;
+            let dy = ((yi as i64) - (yj as i64)).abs() as usize + 1;
+            let area = dx * dy;
+
+            if rectangle_contained(&points, (xi, yi), (xj, yj)) {
+                if area > max_area {
+                    max_area = area;
+                }
+            }
+        }
+    }
+
+    max_area as u64
 }
 
 fn part_one(points: &[(usize, usize)]) -> u64 {
